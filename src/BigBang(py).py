fcf94a16797b353eb7513f6c98d3743de30f68b9
Eelaya Darsini <eelayadarsini@gmail.com>
2023-05-19 00:34:21 +0800

    updtae

---
+++ src/BigBang(py).py
@@ -0,0 +1,16 @@
+import json
+
+result = []
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        result.append("BIGBANG")
+    elif i % 3 == 0:
+        result.append("BIG")
+    elif i % 5 == 0:
+        result.append("BANG")
+    else:
+        result.append(str(i))
+
+with open('output.json', 'w') as outfile:
+    json.dump(result, outfile)
